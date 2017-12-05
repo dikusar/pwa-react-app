e7d7b1c3c08ea3a84f21d6f320c1a83232e162e8
denis.dikusar <denis.dikusar@sintez.co.za>
2017-12-05 19:30:15 +0200

    add packages: react-router, material-ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+import AppBar from 'material-ui/AppBar';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import './App.css';
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+
 class App extends Component {
+
+    getChildContext() {
+                return { muiTheme: getMuiTheme(baseTheme) };
+            }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+            <div className="App">
+
+              <header className="App-header">
+                <AppBar />
+
+
+                <h1 className="App-title">Welcome to React</h1>
+
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+
+              </header>
+
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+              
+            </div>
+      </Router>
     );
   }
 }
+
+// App.childContextTypes = {
+//     muiTheme: React.PropTypes.object.isRequired
+// };
 
 export default App;
