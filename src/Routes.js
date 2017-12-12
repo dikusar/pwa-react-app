8a4725389496cd7a25c66c777fdc41146ee9a34a
Denis Dikusar <lovipochtuarnold@gmail.com>
2017-12-12 02:12:56 +0200

    indent formatted

--- src/Routes.js
+++ src/Routes.js
@@ -9,34 +9,34 @@ import InspectionList from './components/inspection-list'
 import ResponsiveDrawer from './components/responsive-drawer'
 
 const About = () => (
-  <div>
-	<h2>URL About</h2>
-  </div>
+	<div>
+		<h2>URL About</h2>
+	</div>
 )
 
-const routes = [
-  { path: '/',
-    component: InspectionList
-  },
-  { path: '/about',
-    component: About
-  }
+const routes = [{
+        path: '/',
+        component: InspectionList
+    },
+    {
+        path: '/about',
+        component: About
+    }
 ]
 
 const RouterFrame = (route) => (
 	<Route exact path={route.path} component={route.component} />
-)
+	)
 
 export default ({childProps}) =>
 
-	<Router>
-		
-		<div>
-			<ResponsiveDrawer />
-			{routes.map( (route, i) => (
-				<RouterFrame key={i} {...route}/>
-			 ))}
-			
-		</div>
-	</Router>
-;
+<Router>
+
+<div>
+	<ResponsiveDrawer />
+	{routes.map( (route, i) => (
+		<RouterFrame key={i} {...route}/>
+		))}
+
+	</div>
+</Router>;
